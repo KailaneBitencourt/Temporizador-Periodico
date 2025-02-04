15eeb7a783140b160aa776eb910f52c01a1f8d88
Kailane Bitencourt <kailanebitencourtdossantos@gmail.com>
2025-02-03 21:52:50 -0300

    Código da atividade 2

--- freq_03.c
+++ freq_03.c
@@ -3,39 +3,63 @@
 #include "hardware/timer.h" // Inclui a biblioteca para gerenciamento de temporizadores de hardware.
 
 #define LED_PIN_RED 11
+#define LED_PIN_YELLOW 12
+#define LED_PIN_GREEN 13
+
 bool led_on = false;
+
+int mudanca_das_cores = 0;
 
 // Função de callback que será chamada repetidamente pelo temporizador
 // O tipo bool indica que a função deve retornar verdadeiro ou falso para continuar ou parar o temporizador.
 bool repeating_timer_callback(struct repeating_timer *t) {
-    // Imprime uma mensagem na saída serial indicando que 1 segundo se passou.
-    printf("1 segundo passou\n");
-    //Liga ou desliga o led.
-    led_on = !led_on;
-    gpio_put(LED_PIN_RED,led_on);
-    // Retorna true para manter o temporizador repetindo. Se retornar false, o temporizador para.
+    switch(mudanca_das_cores){
+        case 0:
+            gpio_put(LED_PIN_RED, 1);
+            gpio_put(LED_PIN_YELLOW, 0);
+            gpio_put(LED_PIN_GREEN, 0);
+            printf("SINAL VERMELHO!\n");
+            mudanca_das_cores = 1;
+            break;
+        case 1:
+            gpio_put(LED_PIN_RED, 0);
+            gpio_put(LED_PIN_YELLOW, 1);
+            gpio_put(LED_PIN_GREEN, 0);
+            printf("SINAL AMARELO!\n");
+            mudanca_das_cores = 2;
+            break;
+        case 2:
+            gpio_put(LED_PIN_RED, 0);
+            gpio_put(LED_PIN_YELLOW, 0);
+            gpio_put(LED_PIN_GREEN, 1);
+            printf("SINAL VERDE!\n");
+            mudanca_das_cores = 0;
+            break;
+
+    }
     return true;
 }
 
 int main() {
     // Inicializa a comunicação serial, permitindo o uso de funções como printf.
     stdio_init_all();
-
-    // Inicializar o pino GPIO11
+    //inicializando leds
     gpio_init(LED_PIN_RED);
-    gpio_set_dir(LED_PIN_RED,true);
+    gpio_set_dir(LED_PIN_RED, GPIO_OUT);
+    gpio_init(LED_PIN_YELLOW);
+    gpio_set_dir(LED_PIN_YELLOW, GPIO_OUT);
+    gpio_init(LED_PIN_GREEN);
+    gpio_set_dir(LED_PIN_GREEN, GPIO_OUT);
 
     // Declaração de uma estrutura de temporizador de repetição.
     // Esta estrutura armazenará informações sobre o temporizador configurado.
     struct repeating_timer timer;
 
-    // Configura o temporizador para chamar a função de callback a cada 1 segundo.
-    add_repeating_timer_ms(1000, repeating_timer_callback, NULL, &timer);
+    // Configura o temporizador para chamar a função de callback a cada 3 segundo.
+    add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
 
-    // Loop infinito que mantém o programa em execução.
     while (true) {
-        // Pausa de 10 segundos para reduzir o uso da CPU.
-        sleep_ms(10000);
+        sleep_ms(1000);
         printf("rotina de repetição\n");
     }
     return 0;
